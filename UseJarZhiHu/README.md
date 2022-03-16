8492ba779eb5f19c53e8af602dad9970a5d3bfc0
cheng <su.cheng@ledsmart.com>
2022-03-16 14:05:12 -0700

    jar包的一些事儿 https://zhuanlan.zhihu.com/p/82320492

---
+++ UseJarZhiHu/README.md
@@ -0,0 +1,9 @@
+# How to use Jar
+https://zhuanlan.zhihu.com/p/82320492
+
+## 
+```
+```
+
+## checkout the whole Git on windows
+```
---
+++ UseJarZhiHu/jardemo1/src/com/imooc/jardemo1/impl/Teacher.java
@@ -0,0 +1,8 @@
+package com.imooc.jardemo1.impl; 
+
+public class Teacher { 
+  public static void greeting()
+  { 
+    System.out.printf("Welcome!"); 
+  } 
+}
