a5b9f89be4183fd424aa6016eebfee9adf44c0d8
cheng <su.cheng@ledsmart.com>
2022-06-28 09:26:31 -0700

    没时间了，以后在完成吧

--- autoware/AStar/astar.py
+++ autoware/AStar/astar.py
@@ -28,7 +28,36 @@ class Search:
                     frontier.put(next)
                     visited[next] = True
 
+    def SearchRememberPath(self):
+        self.m_graph = Graph(9, 4)
+
+        frontier = Queue()
+        start = (3,1)
+        frontier.put( start )
+        visited = {}            #Come from
+        visited[start] = None
+
+        while not frontier.empty():
+            current = frontier.get()
+            #if current==target:
+                #GetPath()
+                #return
+            for next in self.m_graph.Neighbor4(current[0], current[1]):
+                if next not in visited:
+                    print(next)
+                    frontier.put(next)
+                    visited[next] = current
+
+    def GetPath(self, start, goal, comeFrom):
+        current = goal
+        path = []
+        while current != start:
+            path.append(current)
+            current = came_from[current]
+        path.append(start) # optional
+        path.reverse() # optional
 
 if __name__ == "__main__":
     s = Search()
-    s.BreadthFirstSearch()
+    #s.BreadthFirstSearch()
+    s.SearchRememberPath()
