1b955a4d29cc2fe6b8910a5e694ef303601ca752
cheng <su.cheng@ledsmart.com>
2022-07-12 15:33:17 -0700

    Init Home3数据的采集和存储

---
+++ apArchitect/README.md
@@ -0,0 +1,8 @@
+# 深蓝学院 - 自动驾驶基础构架
+
+## 上课资源
+[上课](https://www.shenlanxueyuan.com/my/course/485)
+
+## 软件资源
+1. [cpp-httplib
+](https://github.com/yhirose/cpp-httplib.git)
---
+++ apArchitect/home3dataCollectStore/Client/Client.cpp
@@ -0,0 +1,47 @@
+//
+//  client.cc
+//
+//  Copyright (c) 2019 Yuji Hirose. All rights reserved.
+//  MIT License
+//
+
+#include <iostream>
+#include "D:/study/cpp-httplib/httplib.h"
+//https://github.com/yhirose/cpp-httplib.git
+
+#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+#undef CPPHTTPLIB_OPENSSL_SUPPORT
+#endif
+
+
+#define CA_CERT_FILE "./ca-bundle.crt"
+
+using namespace std;
+
+int main(void) {
+#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+  httplib::SSLClient cli("localhost", 8080);
+  // httplib::SSLClient cli("google.com");
+  // httplib::SSLClient cli("www.youtube.com");
+  cli.set_ca_cert_path(CA_CERT_FILE);
+  cli.enable_server_certificate_verification(true);
+#else
+  httplib::Client cli("localhost", 8080);
+#endif
+
+  if (auto res = cli.Get("/hi")) {
+    cout << res->status << endl;
+    cout << res->get_header_value("Content-Type") << endl;
+    cout << res->body << endl;
+  } else {
+    cout << "error code: " << res.error() << std::endl;
+#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+    auto result = cli.get_openssl_verify_result();
+    if (result) {
+      cout << "verify error: " << X509_verify_cert_error_string(result) << endl;
+    }
+#endif
+  }
+
+  return 0;
+}
